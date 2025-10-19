3cdaa869a16177ed610034ebe83b3cb1e13a09ff
Cole Puckett <cjp7e@mtmail.mtsu.edu>
2025-10-19 16:16:10 -0500

    feat: created vector for transactions and added AddTransaction function. Will revisit project when coursework covers vectors and structs.

--- expense_tracker.cpp
+++ expense_tracker.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
 #include <print>
+#include <string>
+#include <vector>
+
+// create a transaction list struct (to store multiple variables)
+struct Transaction{
+    std::string description{};
+    double amount{};
+    bool isRevenue{};
+};
+
 
 void PrintMenu(int &);
-/*void AddTransaction();
-void ViewBalance();
+void AddTransaction(std::vector<Transaction> &transactions);
+/*void ViewBalance();
 void ViewTransactions();
 void DeleteTransaction();
 */
@@ -17,9 +27,13 @@ int main(){
     
     // print menu options with a loop
     // use if statements to handle user choices - named function for each option
+    
+    // create transaction vector (to store multiple transactions) 
+    std::vector<Transaction> transactions;
+    
     do { PrintMenu(choice);
         if (choice == 1){
-           // AddTransaction();
+           AddTransaction(transactions);
            std::cout << "\n";
         }
         else if (choice ==2){
@@ -32,12 +46,12 @@ int main(){
             // DeleteTransaction();
             std::cout << "\n";
         } else {
-            std::println("Thank you, goodbye!");
+            std::cout << "\n";
+            std::println("Exiting, goodbye!");
+            std::cout << "\n";
         }
     }
     while ( choice != 5);
-
-    
     
     
     return 0;
@@ -72,15 +86,58 @@ void PrintMenu(int &choice) {
     }
     return;
 }
-/*
-void AddTransaction(){
 
+void AddTransaction(std::vector<Transaction> &transactions){
+
+    // get transaction information and store it in a Transaction struct
+    Transaction t;
+    char type{};
+
+    std::print("Enter transaction Description: ");
+    std::cin.ignore(100, ' ');
+    std::getline(std::cin, t.description);
+    std::print("Is this a revenue or expense?: (R or E): ");
+    std::cin >> type;
+
+    while ( type != 'R' && type != 'E' && type != 'e' && type != 'r'){
+            std::cout << "\n";
+            std::print("Invalid input: please enter R for revenue or E for expense: ");
+            std::cin >> type;
+    }   
+    
+    if ( type == 'R' || type == 'r'){
+        t.isRevenue = true;
+    } else {
+        t.isRevenue = false;
+    }
+
+    std::print("Enter transaction amount: ");
+    std::cin >> t.amount;
+
+    // add transaction information to Transaction vector
+   transactions.push_back(t);
+
+   std::println("Transaction added successfully!");
+    return;
+}
+
+
+// void ViewBalance(std::vector<Transaction> &transactions){
+
+
+
+ //   return;
+// }
+
+
+void ViewTransactions(std::vector<Transaction> &transactions){
+
+    for ( int count{}; count < t; ++count){
+        std::print("Transaction {}: {}, {} of {}", count, t.description, if ( t.type == true ){"Revenue"} else {"Expense"}, t.);
+    }
 
 
     return;
 }
-void ViewBalance()
-void ViewTransactions()
-void DeleteTransaction()
+// void DeleteTransaction()
 
-*/
